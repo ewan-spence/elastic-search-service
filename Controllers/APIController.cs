4edd3a40c72a9605ffaf65582bb57e221e354f06
ewan-spence <ewan_spence@btinternet.com>
2022-02-07 16:58:37 +0000

    Adds endpoint to retrieve API Search Keywq

---
+++ Controllers/APIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ElasticSearchService.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase {
+        private readonly string _APIKey;
+
+        public APIController(IConfiguration config) {
+            _APIKey = config.GetValue<string>("SearchKey");
+        }
+
+        [HttpGet]
+        public IActionResult GetAPIKey() {
+            return Ok(_APIKey);
+        }
+    }
+}
